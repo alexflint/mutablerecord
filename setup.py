49e47b0060eff667fcf43258230c25117f3d86dd
Alex Flint <alex.flint@gmail.com>
2015-07-02 11:25:07 -0700

    updated url in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 
 setup(name='mutablerecord',
       description='recordset framework for small python structures',
-      version='0.11',
+      version='0.12',
       author='Alex Flint',
       author_email='alex.flint@gmail.com',
-      url='...',
+      url='https://github.com/alexflint/mutablerecord',
       packages=['mutablerecord'],
       package_dir={'mutablerecord': '.'},
       )
