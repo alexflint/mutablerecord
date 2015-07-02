3483a6e60b8b18a46b8700fc40074b3a2efd13be
Alex Flint <alex.flint@gmail.com>
2015-07-02 10:45:32 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='mutablerecord',
+      description='recordset framework for small python structures',
+      version='0.1',
+      author='Alex Flint',
+      author_email='alex.flint@gmail.com',
+      url='...',
+      packages=['mutablerecord'],
+      package_dir={'mutablerecord': '.'},
+      )
