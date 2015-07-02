ff235196538f8bce6f40a31794f1230e6a66f104
Alex Flint <alex.flint@gmail.com>
2015-07-02 10:42:37 -0700

    add initial implementation with tests

---
+++ record.py
@@ -0,0 +1,292 @@
+import abc
+import copy
+import collections
+
+# This is a token used symbolically below
+Required = object()
+
+
+class DataError(Exception):
+    """
+    Represents an error encountered while initializing a mutable data type.
+    """
+    pass
+
+
+class Validator(object):
+    """
+    An abstract base class for field validators.
+    """
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, default):
+        """
+        Initialize the validator with a default value
+        """
+        self.default = default
+
+    @abc.abstractmethod
+    def validate(self, value, field_name):
+        """
+        Base classes should override this method and raise an exception if the value is invalid
+        for the given field.
+        """
+        pass
+
+
+class MustSatisfy(Validator):
+    """
+    Represents a custom validator for members of a mutable data type.
+    """
+
+    def __init__(self, predicate, default=Required):
+        super(MustSatisfy, self).__init__(default)
+        self._predicate = predicate
+
+    def validate(self, value, field_name):
+        if not self._predicate(value):
+            raise DataError('%s failed validation (value was %s)' % (field_name, value))
+
+
+class InstanceOf(Validator):
+    """
+    Requires that a member be an instance of a particular class.
+    """
+    def __init__(self, type, default=Required):
+        super(InstanceOf, self).__init__(default)
+        self._type = type
+
+    def validate(self, value, field_name):
+        if not isinstance(value, self._type):
+            raise DataError('%s is not an instance of %s' % (field_name, value))
+
+
+class InstanceOrNone(Validator):
+    """
+    Requires that a member be an instance of a particular class, or None.
+    """
+    def __init__(self, type, default=None):
+        super(InstanceOrNone, self).__init__(default)
+        self._type = type
+
+    def validate(self, value, field_name):
+        if value is not None and not isinstance(value, self._type):
+            raise DataError('%s is not an instance of %s' % (field_name, value))
+
+
+class OneOf(Validator):
+    """
+    Requires that a member be one of a pre-set range of values.
+    """
+    def __init__(self, *possible_values, default=None):
+        super(OneOf, self).__init__(default)
+        self._possible_values = possible_values
+
+    def validate(self, value, field_name):
+        if value is None and self.default is not None:
+            # No value was given, and a default was supplied
+            return
+        if value not in self._possible_values:
+            raise DataError('%s is not a valid value for %s' % (value, field_name))
+
+
+class MutableRecord(object):
+    """
+    Base class for all types created by make_mutable_type.
+    """
+    def __init__(self, members, **kwargs):
+        self._members = members
+        for key, spec in members.items():
+            # Resolve validator and default value
+            if isinstance(spec, Validator):
+                validator = spec
+                default = validator.default
+            else:
+                validator = None
+                default = spec
+
+            # Check for missing required args
+            if key not in kwargs and default is Required:
+                raise DataError('%s is required' % key)
+
+            # Resolve value for this member
+            if key in kwargs:
+                value = kwargs[key]
+            else:
+                value = copy.deepcopy(default)
+
+            # Run validator
+            if validator is not None:
+                validator.validate(value, key)
+
+            # Store
+            setattr(self, key, value)
+
+        # Check for invalid keyword arguments
+        for key in kwargs.keys():
+            if key not in members:
+                raise DataError('%s is not a member of %s' % (key, type(self).__name__))
+
+    def __str__(self):
+        """
+        Get a string representation of this object.
+        """
+        fields = ', '.join('%s=%s' % (k, getattr(self, k)) for k in self._members.keys())
+        return '%s(%s)' % (type(self).__name__, fields)
+
+    def __repr__(self):
+        """
+        Get a string representation of this object.
+        """
+        return str(self)
+
+
+class MutableRecordSet(list):
+    """
+    Base class for all types created by make_mutable_list_type.
+    """
+    def __init__(self, *args):
+        assert 0 <= len(args) <= 1
+        super(MutableRecordSet, self).__init__()
+        if len(args) > 0:
+            for object in args[0]:
+                self.append(object)
+
+    def __getitem__(self, index):
+        value = super(MutableRecordSet, self).__getitem__(index)
+        if isinstance(index, slice):
+            return type(self)(value)
+        else:
+            return value
+
+    def __setitem__(self, index, value):
+        if isinstance(index, slice):
+            objects = list(value)  # make a copy so that we don't throw any away
+            for object in objects:
+                assert isinstance(object, self.value_type)
+            super(MutableRecordSet, self).__setitem__(index, objects)
+        else:
+            assert isinstance(value, self.value_type)
+            super(MutableRecordSet, self).__setitem__(index, value)
+
+    def __getslice__(self, start, end):
+        # only used in python 2
+        return type(self)(super(MutableRecordSet, self).__getslice__(start, end))
+
+    def __setslice__(self, start, end, iterable):
+        # only used in python 2
+        objects = list(iterable)  # make a copy so that we don't throw any away
+        for object in objects:
+            assert isinstance(object, self.value_type)
+        type(self)(super(MutableRecordSet, self).__setitem__(start, end, objects))
+
+    def append(self, object):
+        assert isinstance(object, self.value_type)
+        super(MutableRecordSet, self).append(object)
+
+    def extend(self, iterable):
+        for object in iterable:
+            assert isinstance(object, self.value_type)
+            self.append(object)  # now that we've popped the item off the iterator we must use it
+
+    def insert(self, index, object):
+        assert isinstance(object, self.value_type)
+        super(MutableRecordSet, self).insert(index, object)
+
+    def append_new(self, **kwargs):
+        self.append(self.value_type(**kwargs))
+
+    def insert_new(self, index, **kwargs):
+        self.insert(index, self.value_type(**kwargs))
+
+    def __str__(self):
+        return '[' + '\n '.join(map(str, self)) + ']'
+
+    def __repr__(self):
+        return '[' + ',\n '.join(map(repr, self)) + ']'
+
+
+class FieldView(object):
+    """
+    Represents a view of one field of a MutableDataList subclass.
+    """
+    def __init__(self, container, field):
+        self._container = container
+        self._field = field
+
+    def __len__(self):
+        return len(self._container)
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            return FieldView(self._container[index], self._field)
+        else:
+            return getattr(self._container[index], self._field)
+
+    def __setitem__(self, index, value):
+        if isinstance(index, slice):
+            for item, entry in zip(self._container[index], value):
+                setattr(item, self._field, entry)
+        else:
+            setattr(self._container[index], self._field, value)
+
+    def __iter__(self):
+        for item in self._container:
+            yield getattr(item, self._field)
+
+    def __str__(self):
+        return '[' + ', '.join(map(repr, self)) + ']'
+
+    def __repr__(self):
+        return str(self)
+
+
+class FieldViewDescriptor(object):
+    """
+    A descriptor that resolves to a FieldView for a specific field.
+    """
+    def __init__(self, field):
+        self._field = field
+
+    def __get__(self, instance, type=None):
+        return FieldView(instance, self._field)
+
+
+def make_mutable_type(typename, **members):
+    """
+    Construct a type with mutable members.
+    """
+    def __init__(self, **kwargs):
+        MutableRecord.__init__(self, members, **kwargs)
+
+    bases = (MutableRecord,)
+    dict = {'__init__': __init__}
+    result_type = type(typename, bases, dict)
+    result_type.List = make_mutable_list_type(typename+'List', result_type, members)
+    result_type.Members = members
+    return result_type
+
+
+def make_mutable_list_type(typename, value_type, members):
+    """
+    Construct a type with mutable members.
+    """
+    bases = (MutableRecordSet,)
+    dict = {'value_type': value_type}
+    for key in members.keys():
+        dict[key+'s'] = FieldViewDescriptor(key)
+    return type(typename, bases, dict)
+
+
+class MutableRecordType(type):
+    """
+    A metaclass for constructing mutable record types.
+    """
+    @classmethod
+    def __prepare__(metacls, name, bases): 
+        return collections.OrderedDict()
+
+    def __new__(cls, name, bases, clsdict):
+        if any(base is not object for base in bases):
+            raise TypeError("MutableRecord types may not be have any base class (base was %s)" % base.__name__)
+        return make_mutable_type(name, **clsdict)
---
+++ record_test.py
@@ -0,0 +1,248 @@
+import unittest
+
+from .record import *
+
+class MutableRecordTest(unittest.TestCase):
+	def test_required(self):
+		Foo = make_mutable_type("Foo", a=Required)
+		with self.assertRaises(DataError):
+			f = Foo()
+		f = Foo(a=123)
+		self.assertEqual(f.a, 123)
+
+	def test_instanceof(self):
+		Foo = make_mutable_type("Foo", a=InstanceOf(dict))
+		with self.assertRaises(DataError):
+			f = Foo(a=1)
+		with self.assertRaises(DataError):
+			f = Foo(a=None)
+		with self.assertRaises(DataError):
+			f = Foo()
+		f = Foo(a={})
+		self.assertEqual(f.a, {})
+
+	def test_instanceornone(self):
+		Foo = make_mutable_type("Foo", a=InstanceOrNone(dict))
+		with self.assertRaises(DataError):
+			f = Foo(a=1)
+		f = Foo(a=None)
+		self.assertEqual(f.a, None)
+		f = Foo(a={})
+		self.assertEqual(f.a, {})
+		f = Foo()
+		self.assertEqual(f.a, None)
+
+	def test_oneof(self):
+		Foo = make_mutable_type("Foo", a=OneOf("a", "b", "c"))
+		with self.assertRaises(DataError):
+			f = Foo(a="d")
+		with self.assertRaises(DataError):
+			f = Foo(a=None)
+		with self.assertRaises(DataError):
+			f = Foo()
+		f = Foo(a="a")
+		self.assertEqual(f.a, "a")
+
+	def test_oneof_with_default(self):
+		Foo = make_mutable_type("Foo", a=OneOf("a", "b", "c", default="c"))
+		with self.assertRaises(DataError):
+			f = Foo(a="d")
+		f = Foo()
+		self.assertEqual(f.a, "c")
+		f = Foo(a="a")
+		self.assertEqual(f.a, "a")
+
+	def test_simple_record(self):
+		Foo = make_mutable_type("Foo", a=1, b=2)
+		f1 = Foo(a=123)
+		self.assertEqual(f1.a, 123)
+		self.assertEqual(f1.b, 2)
+
+	def test_complex_record(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam"))
+		f = Foo(a=1, c="ham")
+		self.assertEqual(f.a, 1)
+		self.assertEqual(f.b, 2)
+		self.assertEqual(f.c, "ham")
+
+
+class MutableRecordSetTest(unittest.TestCase):
+	def test_construct_empty(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List()
+		self.assertEqual(len(fs), 0)
+
+	def test_construct_from_list(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List([Foo(a=1), Foo(a=2)])
+		self.assertEqual(len(fs), 2)
+		self.assertEqual(fs[0].a, 1)
+		self.assertEqual(fs[1].a, 2)
+
+	def test_getitem(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List()
+		fs.append_new(a=1, c="ham")
+		fs.append_new(a=2, c="ham")
+		fs.append_new(a=3, c="ham")
+		x = fs[1]
+		self.assertIsInstance(x, Foo)
+		self.assertEqual(x.a, 2)
+
+	def test_setitem(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List()
+		fs.append_new(a=1, c="ham")
+		fs.append_new(a=2, c="ham")
+		fs.append_new(a=3, c="ham")
+		fs[1] = Foo(a=100, c="spam")
+		self.assertEqual(fs[1].a, 100)
+		self.assertEqual(fs[1].b, 2)
+		self.assertEqual(fs[1].c, "spam")
+
+	def test_mutate_item(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List()
+		fs.append_new(a=1, c="ham")
+		fs.append_new(a=2, c="ham")
+		fs.append_new(a=3, c="ham")
+		fs[1].a = 100
+		self.assertEqual(fs[1].a, 100)
+
+	def test_getslice(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List()
+		fs.append_new(a=1, c="ham")
+		fs.append_new(a=2, c="ham")
+		fs.append_new(a=3, c="ham")
+		x = fs[1:]
+		self.assertIsInstance(x, Foo.List)
+		self.assertEqual(len(x), 2)
+		self.assertEqual(x[0].a, 2)
+		self.assertEqual(x[1].a, 3)
+
+	def test_setslice(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List()
+		fs.append_new(a=1, c="ham")
+		fs.append_new(a=2, c="ham")
+		fs.append_new(a=3, c="ham")
+		fs[:2] = [Foo(a=10, c="spam")]
+		self.assertEqual(len(fs), 2)
+		self.assertEqual(fs[0].a, 10)
+		self.assertEqual(fs[1].a, 3)
+
+	def test_append(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List([Foo(a=1)])
+		fs.append(Foo(a=2))
+		self.assertEqual(len(fs), 2)
+		self.assertEqual(fs[0].a, 1)
+		self.assertEqual(fs[1].a, 2)
+
+	def test_append_new(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List()
+		fs.append_new(a=1, c="ham")
+		self.assertEqual(len(fs), 1)
+		self.assertEqual(fs[0].a, 1)
+		self.assertEqual(fs[0].b, 2)
+		self.assertEqual(fs[0].c, "ham")
+
+	def test_insert(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List([Foo(a=1)])
+		fs.insert(0, Foo(a=2))
+		self.assertEqual(len(fs), 2)
+		self.assertEqual(fs[0].a, 2)
+		self.assertEqual(fs[1].a, 1)
+
+	def test_insert_new(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List([Foo(a=1)])
+		fs.insert_new(0, a=2)
+		self.assertEqual(len(fs), 2)
+		self.assertEqual(fs[0].a, 2)
+		self.assertEqual(fs[1].a, 1)
+
+	def test_extend(self):
+		Foo = make_mutable_type("Foo", a=Required, b=2, c=OneOf("ham", "spam", default="ham"))
+		fs = Foo.List([Foo(a=1)])
+		fs.extend([Foo(a=2), Foo(a=3)])
+		self.assertEqual(len(fs), 3)
+		self.assertEqual(fs[0].a, 1)
+		self.assertEqual(fs[1].a, 2)
+		self.assertEqual(fs[2].a, 3)
+
+class FieldViewTest(unittest.TestCase):
+	def test_getitem(self):
+		Foo = make_mutable_type("Foo", x=Required)
+		fs = Foo.List([Foo(x=1), Foo(x=2), Foo(x=3)])
+		xs = fs.xs
+		self.assertEqual(len(xs), 3)
+		self.assertEqual(xs[0], 1)
+		self.assertEqual(xs[1], 2)
+		self.assertEqual(xs[2], 3)
+
+	def test_setitem(self):
+		Foo = make_mutable_type("Foo", x=Required)
+		fs = Foo.List([Foo(x=1), Foo(x=2), Foo(x=3)])
+		xs = fs.xs
+		xs[1] = 100
+		self.assertEqual(fs[1].x, 100)
+
+	def test_getslice(self):
+		Foo = make_mutable_type("Foo", x=Required)
+		fs = Foo.List([Foo(x=1), Foo(x=2), Foo(x=3)])
+		xs = fs.xs[1:]
+		self.assertEqual(len(xs), 2)
+		self.assertEqual(xs[0], 2)
+		self.assertEqual(xs[1], 3)
+
+	def test_setslice(self):
+		Foo = make_mutable_type("Foo", x=Required)
+		fs = Foo.List([Foo(x=1), Foo(x=2), Foo(x=3)])
+		fs.xs[1:] = [100, 200]
+		self.assertEqual(fs[0].x, 1)
+		self.assertEqual(fs[1].x, 100)
+		self.assertEqual(fs[2].x, 200)
+
+	def test_iter(self):
+		Foo = make_mutable_type("Foo", x=Required)
+		fs = Foo.List([Foo(x=1), Foo(x=2), Foo(x=3)])
+		self.assertListEqual(list(iter(fs.xs)), [1, 2, 3])
+
+
+class MetaclassTest(unittest.TestCase):
+	def test_simple_record(self):
+		class Foo(object, metaclass=MutableRecordType):
+			a = 1
+			b = 2
+
+		f1 = Foo(a=123)
+		self.assertEqual(f1.a, 123)
+		self.assertEqual(f1.b, 2)
+
+	def test_complex_record(self):
+		class Foo(object, metaclass=MutableRecordType):
+			a = Required
+			b = 2
+			c = OneOf("ham", "spam")
+
+		f = Foo(a=1, c="ham")
+		self.assertEqual(f.a, 1)
+		self.assertEqual(f.b, 2)
+		self.assertEqual(f.c, "ham")
+
+	def test_required(self):
+		class Foo(object, metaclass=MutableRecordType):
+			a = Required
+
+		with self.assertRaises(DataError):
+			f = Foo()
+		f = Foo(a=123)
+		self.assertEqual(f.a, 123)
+
+
+if __name__ == '__main__':
+	unittest.main()
