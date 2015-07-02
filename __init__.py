0fa0fb24d06d2a922ea84f54b99d0c17de0194f0
Alex Flint <alex.flint@gmail.com>
2015-07-02 11:19:13 -0700

    add external interface to __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from .record import \
+    Required, \
+    DataError, \
+    Validator, \
+    MustSatisfy, \
+    InstanceOf, \
+    InstanceOrNone, \
+    OneOf, \
+    MutableRecord, \
+    MutableRecordSet, \
+    FieldView, \
+    FieldViewDescriptor, \
+    MutableRecordType
